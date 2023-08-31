57566070b86aac162de3a7b1aa5f2ab4c64b952e
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-08-31 21:19:03 +0500

    auth added

--- src/config/DBConnection.js
+++ src/config/DBConnection.js
@@ -4,7 +4,14 @@ const mongoose=require("mongoose");
 const connect=async()=>{
     try{
         const conn=await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log(`database connected at : ${conn.connection.host} ${conn.connection.name}`.cyan);
+        const state = mongoose.connection._readyState;
+        if (state === 1) {
+            console.log(`database connected at : ${conn.connection.host} ${conn.connection.name} Successfully`.cyan);    
+        } else if (state === 2) {
+          console.log(`database connecting please wait .....`);  
+        } else if (state === 3) {
+          console.log(`database disconnecting please wait .....`);
+        }
     }catch(err){
         console.error("Error".red,`${err.message}`.red);
         process.exit(1)
