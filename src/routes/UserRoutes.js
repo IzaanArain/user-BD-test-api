24bf62e3eef4c94b116d25b07bea4badc5416af0
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-08-31 21:24:05 +0500

    auth added

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,14 +1,15 @@
 const express=require("express");
 const { getUser, updateUser, deleteUser, getAllUsers, addUser, loginUser } = require("../controller/UserController");
+const user_token_auth = require("../middleware/Auth");
 
 const router=express.Router();
 
-router.get('/',getAllUsers);
-router.post("/create",addUser);
+router.get('/',user_token_auth,getAllUsers);
+router.post("/create",user_token_auth,addUser);
 router.post("/login",loginUser);
 router.get('/:id',getUser);
-router.put("/update/:id",updateUser);
-router.delete("/delete/:id",deleteUser);
+router.put("/update/:id",user_token_auth,updateUser);
+router.delete("/delete/:id",user_token_auth,deleteUser);
 
 
 module.exports=router;
